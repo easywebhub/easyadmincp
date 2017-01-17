c0b8bb10ac6c85bf1217c75377cf768754026215
vvthinh <vvthinh1545@gmail.com>
2017-01-17 22:16:05 +0700

    #7 fix modal DetailWebsite

--- src/resources/ui/Dialog/DetailWebDlg.ts
+++ src/resources/ui/Dialog/DetailWebDlg.ts
@@ -1,42 +1,48 @@
-import { DialogController } from 'aurelia-dialog';
-import { inject } from 'aurelia-dependency-injection';
-import { WebSiteServices } from '../../../services/WebSite/WebSiteServices';
-@inject(DialogController,WebSiteServices)
+import {
+  DialogController
+} from 'aurelia-dialog';
+import {
+  inject
+} from 'aurelia-dependency-injection';
+import {
+  WebSiteServices
+} from '../../../services/WebSite/WebSiteServices';
+@inject(DialogController, WebSiteServices)
 export class DetailWebDlg {
-    dialogController: DialogController;
-    Web: any;
-    Account: any;
-    webSiteServices:WebSiteServices
-    pendding:boolean=true
-    constructor(dialogController,webSiteServices) {
-        this.dialogController = dialogController
-        this.Web = []
-        this.Account = []
-        this.webSiteServices=webSiteServices
-    }
-    async activate(params) {
-        await this.webSiteServices.DetailWebSite(params.WebsiteId).then(rs=>{
-            this.Web=(rs as any).data;
-            console.log('this.Web',JSON.stringify(this.Web))
-           
-        }).catch(err=>{
-             swal("Notification", `Error Detail Website`, "warning");
-        })
-        
-    }
-   clickProduct()
-    {
-       ($('.product') as any).modal('show');  
-    }
-     clickStagging()
-    {
-       ($('.stagging') as any).modal('show');  
-    }
-     clickAccounts()
-    {
-       ($('.accounts') as any).modal('show');  
-    }
-   
-   
+  dialogController: DialogController;
+  Web: any;
+  Account: any;
+  webSiteServices: WebSiteServices
+  pendding: boolean = true
+  constructor(dialogController, webSiteServices) {
+    this.dialogController = dialogController
+    this.Web = []
+    this.Account = []
+    this.webSiteServices = webSiteServices
+  }
+  async activate(params) {
+    await this.webSiteServices.DetailWebSite(params.WebsiteId).then(rs => {
+      this.Web = (rs as any).data;
+      console.log('this.Web', JSON.stringify(this.Web))
+
+    }).catch(err => {
+      swal("Notification", `Error Detail Website`, "warning");
+    })
+
+  }
+  attached() {
+
+    ($('#listStagging') as any).click(() => {
+      ($('.stagging') as any).modal('show');
+    });
+     ($('#listProduct') as any).click(() => {
+      ($('.product') as any).modal('show');
+    });
+    ($('#listAccounts') as any).click(() => {
+      ($('.accounts') as any).modal('show');
+    });
+  }
+
+
 
 }
